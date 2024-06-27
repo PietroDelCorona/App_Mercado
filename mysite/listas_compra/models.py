cee0b28971524152539788c8bf9a0379e53d8817
Pedro Tian <pedro.tx2001@gmail.com>
2024-06-27 11:24:04 -0300

    Alteração de colunas de tabelas e Inserção de dados no banco de dados

--- mysite/listas_compra/models.py
+++ mysite/listas_compra/models.py
@@ -3,7 +3,7 @@ from usuarios.models import Usuario
 
 class ListaCompra(models.Model):
     data_lista = models.DateField()
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    usuario_id = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Lista de {self.usuario.nome} em {self.data_lista}"
--- mysite/precos/models.py
+++ mysite/precos/models.py
@@ -3,8 +3,8 @@ from produtos.models import Produto
 from mercados.models import Mercado
 
 class Preco(models.Model):
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    mercado = models.ForeignKey(Mercado, on_delete=models.CASCADE)
+    produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    mercado_id = models.ForeignKey(Mercado, on_delete=models.CASCADE)
     data = models.DateField()
     valor = models.DecimalField(max_digits=10, decimal_places=2)
 
