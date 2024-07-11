58bba2baedbe9596eaa3266a4bf194a38457fdb8
Pedro Tian <pedro.tx2001@gmail.com>
2024-07-11 11:45:51 -0300

    Página do usuário retornando nome dele

--- mysite/frontEnd/src/app/pagina-usuario/pagina-usuario.component.ts
+++ mysite/frontEnd/src/app/pagina-usuario/pagina-usuario.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from './usuario.service';
+import { IUsuario } from './IUsuario';
 
 @Component({
   selector: 'app-pagina-usuario',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './pagina-usuario.component.css'
 })
 export class PaginaUsuarioComponent {
+  nomeUsuario: string | undefined;
 
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit() {
+    this.obterUsuario();
+  }
+
+  obterUsuario() {
+    this.usuarioService.obterNomeUsuario().subscribe(
+      (nome: string) => {
+        this.nomeUsuario = nome;
+        return this.nomeUsuario
+      },
+      error => {
+        console.error('Erro ao obter dados do usuário', error);
+      }
+    );
+  }
 }
